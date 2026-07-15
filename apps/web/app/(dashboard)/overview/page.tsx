03c10efe5d551cd7b92be09356a1b793fcaa8f7f
monomeak <sokmeak.sarenn@gmail.com>
2026-07-15 23:06:13 +0700

    feat: enhance dashboard with greeting and formatted date display

--- apps/web/app/(dashboard)/overview/page.tsx
+++ apps/web/app/(dashboard)/overview/page.tsx
@@ -3,16 +3,55 @@ import { getProfileResponse } from "@/src/features/profile/api/profile-api";
 export default function Dashboard() {
   const userProfile = getProfileResponse();
   const fullName = `${userProfile.firstName} ${userProfile.lastName}`.trim();
+  const date = new Date().toUTCString();
 
   return (
-    <h1 className="flex items-center gap-2 text-2xl font-semibold">
-      <span>Hi, {fullName}</span>
-      <span
-        aria-hidden="true"
-        className="inline-block origin-[70%_70%] animate-[wave_1.8s_ease-in-out_finite]"
-      >
-        👋
-      </span>
-    </h1>
+    <main>
+      <span className=" text-muted-foreground">{formatLongDate(date)}</span>
+      <h1 className="flex flex-col items-left gap-2 text-2xl font-semibold pt-2">
+        <span>
+          {getGreeting().toString()}, {userProfile.firstName}
+        </span>
+      </h1>
+    </main>
   );
+}
+
+export function formatLongDate(dateValue: string | Date) {
+  const date = new Date(dateValue);
+
+  const weekday = date.toLocaleDateString("en-GB", {
+    weekday: "long",
+    timeZone: "UTC",
+  });
+
+  const dayMonth = date.toLocaleDateString("en-GB", {
+    day: "2-digit",
+    month: "long",
+    timeZone: "UTC",
+  });
+
+  return `${weekday}, ${dayMonth}`;
+}
+
+export function getGreeting(date: Date = new Date()): string {
+  const hour = date.getHours();
+
+  if (hour >= 5 && hour < 12) {
+    return "Good morning";
+  }
+
+  if (hour === 12) {
+    return "Good noon";
+  }
+
+  if (hour >= 13 && hour < 17) {
+    return "Good afternoon";
+  }
+
+  if (hour >= 17 && hour < 21) {
+    return "Good evening";
+  }
+
+  return "Good night";
 }
