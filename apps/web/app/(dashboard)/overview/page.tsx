74cdc7a6e3b833263129dc3f2b666411185a21ab
monomeak <sokmeak.sarenn@gmail.com>
2026-07-17 23:13:30 +0700

    fix: hydration miss match for date by using react hook to support.

--- apps/web/app/(dashboard)/overview/page.tsx
+++ apps/web/app/(dashboard)/overview/page.tsx
@@ -1,17 +1,22 @@
 "use client";
 import { formatLongDate, getGreeting } from "@/lib/utils";
 import { useCurrentUser } from "@/src/features/auth/hooks/use-current-user";
+import { useEffect, useState } from "react";
 
 export default function Dashboard() {
   const { data: currentUser } = useCurrentUser();
-  const date = new Date().toUTCString();
+  const [date, setDate] = useState("");
+  const [greeting, setGreeting] = useState("");
 
+  useEffect(() => {
+    (setDate(new Date().toUTCString()), setGreeting(getGreeting().toString()));
+  }, []);
   return (
     <main>
       <span className=" text-muted-foreground">{formatLongDate(date)}</span>
       <h1 className="flex flex-col items-left gap-2 text-2xl font-semibold pt-2">
         <span>
-          {getGreeting().toString()}, {currentUser?.firstName}
+          {greeting || "Hello"}, {currentUser?.firstName}
         </span>
       </h1>
     </main>
