99a4817db4e82bf1a302fae4ced61f1165509316
Saren Sokmeak <sokmeak.saren@techostartup.center>
2026-07-23 09:56:33 +0700

    configure react query dev tools for better debuging.

--- apps/web/app/providers.tsx
+++ apps/web/app/providers.tsx
@@ -2,6 +2,9 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState } from "react";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+const env = process.env.NODE_ENV || "development";
+const isDev = env === "development";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(
@@ -17,6 +20,15 @@ export function Providers({ children }: { children: React.ReactNode }) {
   );
 
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      {children}
+      {isDev && (
+        <ReactQueryDevtools
+          initialIsOpen={false}
+          buttonPosition="bottom-right"
+          position="bottom"
+        ></ReactQueryDevtools>
+      )}
+    </QueryClientProvider>
   );
 }
